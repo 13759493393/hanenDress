f21cb6d9127cdd6de1a5df865467a72daaedfd44
Mr_李云龙 <13720200304@163.com>
2018-06-06 16:53:36 +0800

    commit object

--- widget/js/request.js
+++ widget/js/request.js
@@ -1,56 +1,117 @@
-$(document).ready(function () {
+// $(document).ready(function () {
 
-    apiready = function () {
+//主页的apicloud请求
+apiready = function () {
+    api.ajax({
+            url: "http://jliro.hnla.cn/api/Api/home",
+            method: "get",
+            data: ""
+        }, function (ret, err) {
+            alert("进入请求")
+            if (ret) {
+                /*alert(ret)
+                alert(JSON.stringify(ret))
+                alert("请求成功")*/
+                $(".index-brand").html(
+                    "<li><img src='images/brand-img1.jpg' class='img' alt=''>" +
+                    "<a href='" + ret.data.brand.brand1url + "'>" +
+                    "<span><img src='images/ico/brand-ico1.png'></span>" +
+                    "<em>" + ret.data.brand.brand1name + "</em>" +
+                    "<p>" + ret.data.brand.brand1desc + "</p>" +
+                    "</a></li>" +
+                    "<li><img src='images/brand-img2.jpg' class='img' alt=''>" +
+                    "<a href='" + ret.data.brand.brand2url + "'>" +
+                    "<span><img src='images/ico/brand-ico2.png'></span>" +
+                    "<em>" + ret.data.brand.brand2name + "</em>" +
+                    "<p>" + ret.data.brand.brand2desc + "</p>" +
+                    "</a></li>"
+                );
 
-        api.alert("正在发起请求！");
-
-        api.ajax({
-            url: "jliro.postman_collection(all).json",
-            method: "post",
-            headers: "",
-            data: {
-                "pdt_cate": [
-                    {
-                        "id": 1,
-                        "name": "女装",
-                        "en_name": "Clothing",
-                        "img": "http://jliro.hnla.cn/uploads/admin/home_config/20180604/15d4f8d77b5ac4ab9c80ba97a9e61e21.jpg"
-                    },
-                    {
-                        "id": 2,
-                        "name": "时尚配饰",
-                        "en_name": "Accessories",
-                        "img": "http://jliro.hnla.cn/uploads/admin/home_config/20180604/47d2ee55c44824cda625e427aa81674b.jpg"
-                    },
-                    {
-                        "id": 3,
-                        "name": "皮草系列",
-                        "en_name": "Fur series",
-                        "img": "http://jliro.hnla.cn/uploads/admin/home_config/20180604/f7728dcbe294f47739d1d93b33798592.jpg"
-                    }
-                ],
-                "brand": {
-                    "brand1desc": "子品牌1简介",
-                    "brand1img": "http://jliro.hnla.cn/uploads/admin/home_config/20180604/8c10bd2afbd215e4fa4af260238b9029.jpg",
-                    "brand1name": "子品牌1",
-                    "brand1url": "http://jliro.hna.cn",
-                    "brand2desc": "子品牌2简介",
-                    "brand2img": "http://jliro.hnla.cn/uploads/admin/home_config/20180604/80ec0f07ccbfc2f4b6c0413399c29fbe.jpg",
-                    "brand2name": "子品牌2",
-                    "brand2url": "http://jliro.hna.cn"
-                }
-            }, function (ret, err) {
-                if (ret) {
-                    api.alert("请求成功！");
-                } else {
-                    api.alert("请求失败！");
-                }
+                alert("请求完毕")
+            } else {
+                api.alert({msg: ('错误码：' + err.code + '；错误信息：' + err.msg + '网络状态码：' + err.statusCode)});
             }
-        });
+        }
+    )
+};
 
-    };
+/*    //首页请求
+    $.ajax({
+        type: "GET",
+        url: "http://jliro.hnla.cn/api/Api/home",
+        data: "",
+        headers: "",
+        success: function (data) {
 
-});
+            $(".index-brand").html(
+                "<li><img src='images/brand-img1.jpg' class='img' alt=''>" +
+                "<a href='" + data.data.brand.brand1url + "'>" +
+                "<span><img src='images/ico/brand-ico1.png'></span>" +
+                "<em>" + data.data.brand.brand1name + "</em>" +
+                "<p>" + data.data.brand.brand1desc + "</p>" +
+                "</a></li>" +
+                "<li><img src='images/brand-img2.jpg' class='img' alt=''>" +
+                "<a href='" + data.data.brand.brand2url + "'>" +
+                "<span><img src='images/ico/brand-ico2.png'></span>" +
+                "<em>" + data.data.brand.brand2name + "</em>" +
+                "<p>" + data.data.brand.brand2desc + "</p>" +
+                "</a></li>"
+            );
+        },
+        error: function () {
+            alert("请求失败")
+        }
+    })
+
+    //加盟中心请求
+    $.ajax({
+        type: "GET",
+        url: "http://jliro.hnla.cn/api/Api/join",
+        data: "",
+        headers: "",
+        success: function (data) {
+            $(".joinCenter_container").html(data.data.html);
+            $(".page-banner img").attr("src", data.data.banner);
+        },
+        error: function () {
+            alert("请求失败")
+        }
+    })
+
+    //关于我们请求
+    $.ajax({
+        type: "GET",
+        url: "http://jliro.hnla.cn/api/Api/about",
+        data: "",
+        headers: "",
+        success: function (data) {
+            $(".forUsContent_container").html(data.data.html);
+            $(".page-banner img").attr("src", data.data.banner);
+        },
+        error: function () {
+            alert("请求失败")
+        }
+    })
+
+    //服务中心请求
+    $.ajax({
+        type: "GET",
+        url: "http://jliro.hnla.cn/api/Api/service",
+        data: "",
+        headers: "",
+        success: function (data) {
+            $(".serviceCenter_container").html(data.data.html);
+            $(".page-banner img").attr("src", data.data.banner);
+        },
+        error: function () {
+            alert("请求失败")
+        }
+    });*/
+
+// });
+
+
+
 
 
 
